1f66e8974e17d0cdadfbec39f637abe673a3c947
Ben Lengerich <blengerich@gmail.com>
2022-06-12 18:16:06 -0400

    Create preprocessing_utils.py

---
+++ ebm_utils/preprocessing_utils.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def fillna_unknown_dtype_col(X):
+    if X.dtype == np.int or X.dtype == np.int64:
+        X = X.fillna(value=-1)
+    elif X.dtype == np.float:
+        X = X.fillna(value=-1)
+    elif X.dtype == np.bool:
+        X = X.fillna(value=False)
+    elif X.dtype == np.object:
+        X = X.fillna(value='missing')
+    else:
+        print(X.dtype)
+    return X
