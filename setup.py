2b892b4e18c85ec70059a66a14d227cbf0d71968
Ben Lengerich <blengerich@gmail.com>
2023-11-30 08:12:42 -0500

    bump version

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ Intalling EBM Utils.
 
 from setuptools import setup, find_packages
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 setup(
     name='ebm_utils',
     packages=find_packages(),
     version=VERSION,
     author='Ben Lengerich',
-    url='github.com/blengerich/ebm_utils',
+    url='https://github.com/blengerich/ebm_utils',
     install_requires=[
         'scikit-learn',
         'matplotlib',
